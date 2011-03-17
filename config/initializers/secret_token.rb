a0dc8a0404bb5b03ad525b8ecb4f2aa7a8e3b9eb
Min-Chang Yu <minchang.yu@gmail.com>
2011-03-17 15:18:25 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dwr::Application.config.secret_token = 'cbba2eca136ffd23b201ee4f3f783ea3e5f8a5b69c7a0605978aeaadfe06ae6fa3378aae0b2c347fe437948bf5a6fcf6f6652949fa920335b9e2dd1837dfd09e'
